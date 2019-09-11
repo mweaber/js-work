27c36d555e493082064d904ff369510fade07dcf
Matthew Weaber <matthewweaber89@gmail.com>
2019-09-11 18:16:32 -0400

    math and updating UI

--- project 2/app.js
+++ project 2/app.js
@@ -5,4 +5,31 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 function calculateResults(e) {
     e.preventDefault();
     console.log('Calculating');
+
+    //UI Variables 
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    // Math
+    const principle = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    //Monthly Payments
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principle * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principle).toFixed(2);
+    } else {
+        console.log('Please check your numbers');
+    }
+
+     
 }
